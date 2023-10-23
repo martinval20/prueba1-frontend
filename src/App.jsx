d0e2136a5b76345df2e53bdf5b208cb563a83f36
martinval20 <martinval2000@hotmail.com>
2023-10-22 21:51:01 -0500

    Navegación con botón

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,7 @@ function App() {
     <div className="card-main">
       <div className="card-title">Colores</div>
       <ColorList/>
-      <div className="btn-nav">
-        <button className="btn-atras"> &lt; Anterior</button>
-        <button className="btn-siguiente">Siguiente &gt;</button>
-      </div>
+      
 
     </div>
   );
--- src/components/ColorList.jsx
+++ src/components/ColorList.jsx
@@ -3,27 +3,44 @@ import { useEffect, useState } from "react";
 import Color from "./Color";
 import Loader from "./common/Loader/Loader";
 
+function NavPage(props) {
+  return (
+    <div className="btn-nav">
+      <button className="btn-atras" disabled={props.page==1} onClick={() => props.setPage(1)}>
+        {" "}
+        &lt; Anterior
+      </button>
+      <button className="btn-siguiente" disabled={props.page==2} onClick={() => props.setPage(2)}>
+        Siguiente &gt;
+      </button>
+    </div>
+  );
+}
 function ColorList() {
   const [colors, setColors] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("https://reqres.in/api/colors");
+      const response = await fetch(`https://reqres.in/api/colors?page=${page}`);
       const data = await response.json();
       setLoading(false);
       setColors(data.data);
     }
     fetchData();
-  }, []);
+  }, [page]);
 
   return loading ? (
     <Loader />
   ) : (
-    <div className="container">
-      {colors.map((colors) => {
-        return <Color key={colors.id} colors={colors} />;
-      })}
+    <div>
+      <div className="container">
+        {colors.map((colors) => {
+          return <Color key={colors.id} colors={colors} />;
+        })}
+      </div>
+      <NavPage page={page} setPage={setPage}/>
     </div>
   );
 }
