1f53f4705ce7f1fcec4db06f3494517312a1f43b
martinval20 <martinval2000@hotmail.com>
2023-10-23 19:59:43 -0500

    Correción en la copia del código del color

--- src/components/ColorList.jsx
+++ src/components/ColorList.jsx
@@ -6,15 +6,18 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 function ColorList() {
   const [colors, setColors] = useState([]);
   const [page, setPage] = useState(1);
-  const [isCopied, setisCopied] = useState(true);
-  const [colorColor, setColorColor] = useState("");
+  const [copied, setCopied] = useState(true);
+  const [colorColor, setColorColor] = useState();
 
-  function colorSelected(colors) {
-    const colorCopy = colors.color;
-    setColorColor(colorCopy);
-    setisCopied(!isCopied);
+  function copiado() {
+    setCopied(!copied);
   }
-
+  async function colorSelected(colors) {
+    const colorCopied = colors.color;
+    setColorColor(colorCopied);
+    copiado();
+    console.log(colorCopied);
+  }
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(`https://reqres.in/api/colors?page=${page}`);
@@ -24,14 +27,18 @@ function ColorList() {
     fetchData();
   }, [page]);
 
-  return isCopied ? (
+  return copied ? (
     <div>
       <div className="container">
         {colors.map((colors) => {
           return (
-            <CopyToClipboard key={colors.id} text={`${colorColor}`}>
-              <div onClick={() => colorSelected(colors)} className="card" style={{ background: `${colors.color}` }}>
-                <p className="year" >{colors.year} </p>
+            <CopyToClipboard key={colors.id} text={colors.color}>
+              <div
+                onClick={() => colorSelected(colors)}
+                className="card"
+                style={{ background: `${colors.color}` }}
+              >
+                <p className="year">{colors.year} </p>
                 <p className="colorName">{colors.name}</p>
                 <p className="colorCode">{colors.color} </p>
                 <p className="colorPatone">{colors.pantone_value}</p>
@@ -50,7 +57,11 @@ function ColorList() {
           .map((colors) => {
             return (
               <CopyToClipboard key={colors.id} text={colorColor}>
-                <div onClick={() => colorSelected(colors)} className="card2" style={{ background: `${colors.color}` }}>
+                <div
+                  onClick={() => copiado()}
+                  className="card2"
+                  style={{ background: `${colors.color}` }}
+                >
                   <p className="year">{colors.year} </p>
                   <p className="copiado">¡Copiado!</p>
                   <p className="colorPatone">{colors.pantone_value}</p>
@@ -59,7 +70,6 @@ function ColorList() {
             );
           })}
       </div>
-      <NavButton page={page} setPage={setPage} />
     </div>
   );
 }
